a186d45db6b083da0d8d4ac1a5edc6a83ed26bf3
nithyajai <nithyajai23@gmail.com>
2022-04-24 15:30:24 +0200

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World first prj jenkin");
+        primaryStage.setTitle("Hello World first pro jenkin");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
